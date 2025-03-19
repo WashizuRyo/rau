36c13fc21f570bc915a773dcf269d23b89b950e1
washizu <mokamikan1027@gmail.com>
2025-03-19 18:59:29 +0900

    test: ログイン状態に応じたレイアウトリンクのテスト実装

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -14,4 +18,31 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'title', full_title("Sign up")
   end
+
+  test 'layout links when user logged in' do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
+
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+  end
+
+  test 'layout links when user not logged in' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', login_path
+
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+  end
+
 end
