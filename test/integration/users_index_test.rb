0db8a4efe1b9757045b46bae3faf62cb25e758f8
washizu <mokamikan1027@gmail.com>
2025-03-24 00:19:28 +0900

    fix: Userのindexページにおいて, アカウント有効化されたユーザのみを表示する実装に変更したことに伴い, テストコードを修正

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,18 +1,26 @@
 require "test_helper"
 
-class UsersIndexTest < ActionDispatch::IntegrationTest
+class UsersIndex < ActionDispatch::IntegrationTest
+
   def setup
-    @user = users(:michael)
     @admin = users(:michael)
     @non_admin = users(:archer)
   end
+end
+
+class UsersIndexAdmin < UsersIndex
+  def setup
+    super
+    log_in_as(@admin)
+    get users_path
+  end
 
   test 'index including pagination' do
-    log_in_as(@user)
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    User.paginate(page: 1).each do |user|
+    User.where(activated: true).paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
   end
@@ -22,7 +30,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users = User.where(activated: true).paginate(page: 1)
     first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin
@@ -33,6 +41,22 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       assert_redirected_to users_url
     end
   end
+
+  test 'should display only activated users' do
+    # ページにいる最初のユーザーを無効化する。
+    # 無効なユーザーを作成するだけでは、
+    # Railsで最初のページに表示される保証がないので不十分
+    User.paginate(page: 1).first.toggle!(:activated)
+    # /usersを再度取得して、無効化済みのユーザーが表示されていないことを確かめる
+    get users_path
+    # 表示されているすべてのユーザーが有効化済みであることを確かめる
+    assigns(:users).each do |user|
+      assert user.activated
+    end
+  end
+end
+
+class UserNonAdminIndexTest < UsersIndex
 
   test 'index as non-admin' do
     log_in_as(@non_admin)
