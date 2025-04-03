ab1f95d3845725d1479bb4047770f1f0b43fef54
washizu <mokamikan1027@gmail.com>
2025-04-03 21:02:27 +0900

    feat: ユーザのフォローボタンを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   get 'sessions/new'
   root 'static_pages#home'
-  get '/help', to: "static_pages#help"
-  get '/about', to: "static_pages#about"
-  get '/contact', to: "static_pages#contact"
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only: %i[new create edit update]
   resources :microposts, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
   get '/microposts', to: 'static_pages#home'
 end
